ef34696e8c7ddf40a2217322847bd11d0ea7e343
HussainQH <hussainqh@gmail.com>
2021-11-22 21:19:09 +0300

    Fetch Profiles

--- Components/Navigation/index.js
+++ Components/Navigation/index.js
@@ -2,15 +2,20 @@ import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import Signin from "../Authentication/Signin";
 import Signup from "../Authentication/Signup";
+import ProfileDetail from "../Profile/ProfileDetail";
+import ProfileList from "../Profile/ProfileList";
+
 import Welcome from "../Welcome/Welcome";
 
 const RootNavigator = () => {
   const { Navigator, Screen } = createStackNavigator();
   return (
-    <Navigator initialRouteName="Signin">
+    <Navigator initialRouteName="ProfileList">
       <Screen name="Signup" component={Signup} />
       <Screen name="Signin" component={Signin} />
       <Screen name="Welcome" component={Welcome} />
+      <Screen name="ProfileList" component={ProfileList} />
+      <Screen name="ProfileDetail" component={ProfileDetail} />
     </Navigator>
   );
 };
---
+++ Components/Profile/ProfileItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { Text, Image, View } from "react-native";
+
+import { Button } from "native-base";
+import { baseURL } from "../../Stores/instance";
+
+const ProfileItem = ({ profile, navigation }) => {
+  console.log(profile);
+  return (
+    <View>
+      <Image
+        style={{ width: 100, height: 100 }}
+        source={{ uri: baseURL + profile.image }}
+      />
+
+      <Text> {profile.description} </Text>
+      <Button
+        onPress={() =>
+          navigation.navigate("ProfileDetail", { profile: profile })
+        }
+      >
+        Details
+      </Button>
+    </View>
+  );
+};
+
+export default ProfileItem;
